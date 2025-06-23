168ccb334a852db10e870e9bd3a0a42325adeafa
Francis Kaela Bonhayag <139144840+kaebogs@users.noreply.github.com>
2025-06-23 16:44:53 +0800

    Delete ShopNa-main directory

--- ShopNa-main/backend/middleware/catchAsyncError.js
+++
@@ -1,3 +0,0 @@
-// to automatically catch errors and pass them to Express error handling middleware.
-export default (controllerFunction) => (req, res, next) => 
-    Promise.resolve(controllerFunction(req, res, next)).catch(next);
